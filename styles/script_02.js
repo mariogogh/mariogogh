6b8fca55f4a098d7dbf6f021df437ecb3deee40c
Mariogogh <hello@mariogogh.com>
2024-11-24 19:54:21 -0300

    up
    changes

--- styles/script_02.js
+++ styles/script_02.js
@@ -1,19 +1,22 @@
-let links = document.querySelectorAll('.menu-item');
-let linkImages = document.querySelectorAll('.hover-reveal__img');
+let attached = false;
 
-links.forEach((link, idx) => {
-    link.addEventListener('mousemove', (e) => {
-        link.children[1].style.opacity = 1;
-        link.children[0].Style.zIndex = 3;
-        link.children[1].style.transform = translate(${ e.clientX - 300 }px, - ${ e.clientY / 3 }px) rotate(${ e.clientX / 20 }deg)';
-        linkImages[idx].Style.transform = "scale(1,1)"
-        link.style.zIndex = 2
-    })
+let imageContainer = document.querySelector("#image_projeto");
 
-    link.addEventListener('mouseleave', (e) => {
-        link.children[1].style.opacity = 0;
-        link.children[1].style.transform = 'translete(${-e.clientX}px, -300px)''
-        LinkImages[idx].Style.transform = 'scale(0.8, 0.8)';
-        link.style.zIndex = 0
-    })
-})
+const followMouse = (event) => {
+    imageContainer.style.left = event.x + "px";
+    imageContainer.style.top = event.y + "px";
+}
+
+function showImage() {
+    if (!attached) {
+        attached = true;
+        imageContainer.style.display = "block";
+        document.addEventListener("pointermove", followMouse);
+    }
+}
+
+function hideImage() {
+    attached = false;
+    imageContainer.style.display = "";
+    document.removeEventListener("pointermove", followMouse);
+}
