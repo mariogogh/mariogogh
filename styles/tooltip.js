60da6c78906d3ed41f4a9cf628c6a1c5dd9f8fd1
Mariogogh <hello@mariogogh.com>
2024-12-17 23:47:21 -0300

    Few Ajustes

---
+++ styles/tooltip.js
@@ -0,0 +1,10 @@
+var tooltips = document.querySelectorAll('.tooltip span');
+
+window.onmousemove = function (e) {
+    var x = (e.clientX + 20) + 'px',
+        y = (e.clientY + 20) + 'px';
+    for (var i = 0; i < tooltips.length; i++) {
+        tooltips[i].style.top = y;
+        tooltips[i].style.left = x;
+    }
+};
